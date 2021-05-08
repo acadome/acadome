9c9337a022a847c80efce74b13ff2defe6eded2a
Rishabh Ballal <rishabhballal@gmail.com>
2021-05-08 05:57:57 +0530

    add: mail init

--- acadome/__init__.py
+++ acadome/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_mongoengine import MongoEngine
+from flask_mail import Mail
 from .config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = MongoEngine(app)
+mail = Mail(app)
 
 from . import views
