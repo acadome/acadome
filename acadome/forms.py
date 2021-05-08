892b8929c8c02bbe49608a2578cc0ca26a951f97
Rishabh Ballal <rishabhballal@gmail.com>
2021-05-08 16:55:40 +0530

    mod: name

--- acadome/forms.py
+++ acadome/forms.py
@@ -1,13 +1,12 @@
 from wtforms import Form, StringField, FileField
-from wtforms.validators import InputRequired, Length, Email, ValidationError
+from wtforms.validators import Length, Email, ValidationError
 
 def input_required(form, field):
     if len(field.data) == 0:
         raise ValidationError('This field is required.')
 
 class PublishForm(Form):
-    fname = StringField(label='First name *', validators=[Length(max=20), input_required])
-    lname = StringField(label='Last name *', validators=[Length(max=20), input_required])
-    email = StringField(label='Email *', validators=[Email(), Length(max=40), input_required])
-    inst = StringField(label='Research institute', validators=[Length(max=60)])
-    file = FileField(label='Upload PDF file *')
+    name = StringField(label='Name *', validators=[Length(max=64), input_required])
+    email = StringField(label='Email *', validators=[Email(), Length(max=64), input_required])
+    inst = StringField(label='Research institute', validators=[Length(max=128)])
+    file = FileField(label='Upload')
