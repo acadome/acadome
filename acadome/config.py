0d21a0ca95122c15437e0a64adfdfa5e260496b0
Rishabh Ballal <rishabhballal@gmail.com>
2021-05-15 14:57:05 +0530

    add: env vars

---
+++ acadome/config.py
@@ -0,0 +1,18 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv(os.path.join(os.path.dirname(__file__), '.env'))
+
+class Config:
+    SECRET_KEY = os.environ.get("SECRET_KEY")
+    FLASK_APP = 'wsgi.py'
+    STATIC_FOLDER = 'static'
+    TEMPLATES_FOLDER = 'templates'
+    MONGODB_SETTINGS = {
+        'host': os.environ.get("MONGODB_HOST"),
+    }
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 587
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    MAIL_USE_TLS = True
