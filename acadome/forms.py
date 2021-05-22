327d465ed19953745d709ac7b302fa9aa6dfff47
Rishabh Ballal <rishabhballal@gmail.com>
2021-05-22 12:20:54 +0530

    mod: validators

--- acadome/forms.py
+++ acadome/forms.py
@@ -3,15 +3,15 @@ from wtforms.validators import Length, Email, ValidationError, Regexp
 
 def input_required(form, field):
     if len(field.data) == 0:
-        raise ValidationError('This field is required.')
+        raise ValidationError('')
 
 class PublishForm(Form):
     name = StringField(label='Name *', validators=[Length(max=64), input_required, Regexp('^[a-zA-Z \-\']+$')])
-    email = StringField(label='Email *', validators=[Email(), Length(max=64), input_required, Regexp('^[a-zA-Z0-9_\-\.]+@[a-zA-Z0-9\-\.]+\.[a-zA-Z]+$')])
-    affil = StringField(label='Affiliation', validators=[Length(max=128), Regexp('^[a-zA-Z0-9 \,\-\']*$')])
+    email = StringField(label='Email *', validators=[Email(), Length(max=254), input_required, Regexp('^[a-zA-Z0-9_\-\.]+@[a-zA-Z0-9\-\.]+\.[a-zA-Z]+$')])
+    affiliation = StringField(label='Affiliation', validators=[Length(max=256), Regexp('^[a-zA-Z0-9 \,\.\-\']*$')])
     file = FileField(label='Upload')
 
 class ContactForm(Form):
     name = StringField(label='Name *', validators=[Length(max=64), input_required, Regexp('^[a-zA-Z \-\']+$')])
-    email = StringField(label='Email *', validators=[Email(), Length(max=64), input_required, Regexp('^[a-zA-Z0-9_\-\.]+@[a-zA-Z0-9\-\.]+\.[a-zA-Z]+$')])
-    query = TextAreaField(label='Query *', validators=[Length(max=512), input_required, Regexp('^[a-zA-Z0-9 \,\.\-\']+$')])
+    email = StringField(label='Email *', validators=[Email(), Length(max=254), input_required, Regexp('^[a-zA-Z0-9_\-\.]+@[a-zA-Z0-9\-\.]+\.[a-zA-Z]+$')])
+    query = TextAreaField(label='Query *', validators=[Length(max=1024), input_required, Regexp('^[a-zA-Z0-9 \,\.\-\'\r\n]+$')])
