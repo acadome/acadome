3eed80202da31ede379db63a3a3d3abad82242a7
Rishabh Ballal <rishabhballal@gmail.com>
2021-05-30 06:42:32 +0530

    add: checkbox

--- acadome/forms.py
+++ acadome/forms.py
@@ -1,8 +1,12 @@
-from wtforms import Form, StringField, FileField, TextAreaField
+from wtforms import Form, StringField, FileField, TextAreaField, BooleanField
 from wtforms.validators import Length, Email, ValidationError, Regexp
 
 def input_required(form, field):
     if len(field.data) == 0:
+        raise ValidationError('')
+
+def checkbox(form, field):
+    if not field.data:
         raise ValidationError('')
 
 class PublishForm(Form):
@@ -10,6 +14,7 @@ class PublishForm(Form):
     email = StringField(label='Email *', validators=[Email(), Length(max=254), input_required, Regexp('^[a-zA-Z0-9_\-\.]+@[a-zA-Z0-9\-\.]+\.[a-zA-Z]+$')])
     affiliation = StringField(label='Affiliation', validators=[Length(max=256), Regexp('^[a-zA-Z0-9 \,\.\-\']*$')])
     file = FileField(label='Upload')
+    agreement = BooleanField(validators=[checkbox])
 
 class ContactForm(Form):
     name = StringField(label='Name *', validators=[Length(max=64), input_required, Regexp('^[a-zA-Z \-\']+$')])
