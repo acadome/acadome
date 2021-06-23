beb307f927ccd3bef5e81f63e2c0d0ceabedb8cf
Rishabh Ballal <rishabhballal@gmail.com>
2021-06-23 12:15:30 +0530

    add: hide flash

--- acadome/static/js/main.js
+++ acadome/static/js/main.js
@@ -1,9 +1,16 @@
 // NAV
 const nav = document.querySelectorAll('nav a');
 nav.forEach(li => {
-  if (window.location.href == li.href) {
+  if (window.location.pathname == li.getAttribute('href')) {
     li.style.borderBottom = '#000 solid 2px';
     li.style.pointerEvents = 'none';
     li.style.cursor = 'default';
   }
 });
+
+const flash = document.getElementById('hide-flash');
+if (flash) {
+  flash.addEventListener('click', () => {
+    document.getElementById('flash').style.display = 'none';
+  });
+}
