566f96d8759f06c7996a599cce8e49ea33f39551
Rishabh Ballal <rishabhballal@gmail.com>
2021-06-24 17:28:46 +0530

    del: default email validator

--- acadome/gen/forms.py
+++ acadome/gen/forms.py
@@ -1,7 +1,7 @@
 from wtforms import Form, StringField, FileField, TextAreaField, BooleanField
-from wtforms.validators import Length, Email, ValidationError, Regexp
+from wtforms.validators import Length, ValidationError, Regexp
 
-def input_required(form, field):
+def required(form, field):
     if len(field.data) == 0:
         raise ValidationError('')
 
@@ -10,13 +10,78 @@ def checkbox(form, field):
         raise ValidationError('')
 
 class PublishForm(Form):
-    name = StringField(label='Name *', validators=[Length(max=64), input_required, Regexp('^[a-zA-Z \-\']+$')])
-    email = StringField(label='Email *', validators=[Email(), Length(max=254), input_required, Regexp('^[a-zA-Z0-9_\-\.]+@[a-zA-Z0-9\-\.]+\.[a-zA-Z]+$')])
-    affiliation = StringField(label='Affiliation', validators=[Length(max=256), Regexp('^[a-zA-Z0-9 \,\.\-\']*$')])
-    file = FileField(label='Upload')
-    agreement = BooleanField(validators=[checkbox])
+    name = StringField(
+        label='Name *',
+        validators=[
+            required,
+            Length(max=64),
+            Regexp(
+                '^[a-zA-Z \-\']+$',
+                message='Invalid characters in name.'
+            )
+        ]
+    )
+    email = StringField(
+        label='Email *',
+        validators=[
+            required,
+            Length(max=254),
+            Regexp(
+                '^[a-zA-Z0-9_\-\.]+@[a-zA-Z0-9\-\.]+$',
+                message='Invalid email address.'
+            )
+        ]
+    )
+    affiliation = StringField(
+        label='Affiliation',
+        validators=[
+            Length(max=256),
+            Regexp(
+                '^[a-zA-Z0-9 \,\.\-\']*$',
+                message='Invalid characters in affiliation.'
+            )
+        ]
+    )
+    file = FileField(
+        label='Upload'
+    )
+    agreement = BooleanField(
+        validators=[
+            checkbox
+        ]
+    )
 
 class ContactForm(Form):
-    name = StringField(label='Name *', validators=[Length(max=64), input_required, Regexp('^[a-zA-Z \-\']+$')])
-    email = StringField(label='Email *', validators=[Email(), Length(max=254), input_required, Regexp('^[a-zA-Z0-9_\-\.]+@[a-zA-Z0-9\-\.]+\.[a-zA-Z]+$')])
-    query = TextAreaField(label='Query *', validators=[Length(max=1024), input_required, Regexp('^[a-zA-Z0-9 \,\.\-\'\r\n]+$')])
+    name = StringField(
+        label='Name *',
+        validators=[
+            required,
+            Length(max=64),
+            Regexp(
+                '^[a-zA-Z \-\']+$',
+                message='Invalid characters in name.'
+            )
+        ]
+    )
+    email = StringField(
+        label='Email *',
+        validators=[
+            required,
+            Length(max=254),
+            Regexp(
+                '^[a-zA-Z0-9_\-\.]+@[a-zA-Z0-9\-\.]+$',
+                message='Invalid email address.'
+            )
+        ]
+    )
+    query = TextAreaField(
+        label='Query *',
+        validators=[
+            required,
+            Length(max=1024),
+            Regexp(
+                '^[a-zA-Z0-9 \,\.\-\'\r\n]+$',
+                message='Invalid characters in query.'
+            )
+        ]
+    )
