70d08136a0304a2636d1c2773e0a7bb2929eeaca
Rishabh Ballal <rishabhballal@gmail.com>
2021-07-12 17:09:33 +0530

    mod: formatting

--- acadome/articles/__init__.py
+++ acadome/articles/__init__.py
@@ -1,5 +1,11 @@
 from flask import Blueprint
 
-articles = Blueprint('articles', __name__, template_folder='templates', static_folder='static', static_url_path='/articles/static')
+articles = Blueprint(
+    'articles',
+    __name__,
+    template_folder='templates',
+    static_folder='static',
+    static_url_path='/articles/static'
+)
 
 from acadome.articles import views
