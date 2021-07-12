74840167c0f32c578a7727a2db6f11a99d5088e7
Rishabh Ballal <rishabhballal@gmail.com>
2021-07-12 16:10:58 +0530

    add: users blueprint

--- acadome/__init__.py
+++ acadome/__init__.py
@@ -1,17 +1,24 @@
 from flask import Flask
 from pymongo import MongoClient
+from flask_bcrypt import Bcrypt
 from flask_mail import Mail
 from acadome.config import Config
+from acadome.manager import UserManager
 
 app = Flask(__name__)
 app.config.from_object(Config)
-client = MongoClient(app.config['MONGODB_HOST'])
+
+client = MongoClient(app.config['MONGODB_URI'])
 db = client.acadome_db
+bcrypt = Bcrypt(app)
 mail = Mail(app)
+um = UserManager(app)
 
 from acadome.gen import gen
 from acadome.articles import articles
+from acadome.users import users
 from acadome.errors import errors
 app.register_blueprint(gen)
 app.register_blueprint(articles)
+app.register_blueprint(users)
 app.register_blueprint(errors)
